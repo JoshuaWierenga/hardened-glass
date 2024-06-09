fcde0c8544ad77359c8ffc88b9e3080ef11e2a3b
Joshua Wierenga <joshuagit@joshuawierenga.tech>
2024-06-09 23:48:27 +1000

    Add item to same item group as glass
    Would make sense to have different colours like glass and possibly also panes.
    If so moving to a custom item group is probably a good idea.

--- src/main/kotlin/au/com/programmingclub/hardenedglass/HardenedGlass.kt
+++ src/main/kotlin/au/com/programmingclub/hardenedglass/HardenedGlass.kt
@@ -1,7 +1,9 @@
 package au.com.programmingclub.hardenedglass
 
 import net.fabricmc.api.ModInitializer
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents
 import net.minecraft.item.Item
+import net.minecraft.item.ItemGroups
 import net.minecraft.util.Identifier
 import net.minecraft.registry.Registries
 import net.minecraft.registry.Registry
@@ -19,5 +21,9 @@ object HardenedGlass : ModInitializer {
     // However, some things (like resources) may still be uninitialized.
     // Proceed with mild caution.
     Registry.register(Registries.ITEM, Identifier(namespace, "hardened_glass"), HardenedGlassItem)
+
+    ItemGroupEvents.modifyEntriesEvent(ItemGroups.COLORED_BLOCKS).register({content ->
+      content.add(HardenedGlassItem)
+    })
   }
 }
