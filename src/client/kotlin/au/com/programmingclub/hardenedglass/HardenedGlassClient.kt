21c4a0b80e144adc9d7903d8c636a86d61e7ba63
Joshua Wierenga <joshuagit@joshuawierenga.tech>
2024-06-09 22:20:20 +1000

    Add item for hardened glass

--- src/client/kotlin/au/com/programmingclub/hardenedglass/HardenedGlassClient.kt
+++ src/client/kotlin/au/com/programmingclub/hardenedglass/HardenedGlassClient.kt
@@ -3,7 +3,7 @@ package au.com.programmingclub.hardenedglass
 import net.fabricmc.api.ClientModInitializer
 
 object HardenedGlassClient : ClientModInitializer {
-	override fun onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-	}
+  override fun onInitializeClient() {
+    // This entrypoint is suitable for setting up client-specific logic, such as rendering.
+  }
 }
--- src/main/kotlin/au/com/programmingclub/hardenedglass/HardenedGlass.kt
+++ src/main/kotlin/au/com/programmingclub/hardenedglass/HardenedGlass.kt
@@ -1,15 +1,23 @@
 package au.com.programmingclub.hardenedglass
 
 import net.fabricmc.api.ModInitializer
+import net.minecraft.item.Item
+import net.minecraft.util.Identifier
+import net.minecraft.registry.Registries
+import net.minecraft.registry.Registry
 import org.slf4j.LoggerFactory
 
 object HardenedGlass : ModInitializer {
-    private val logger = LoggerFactory.getLogger("hardenedglass")
+  private val namespace = "hardenedglass"
 
-	override fun onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-		logger.info("Hello Fabric world!")
-	}
+  private val logger = LoggerFactory.getLogger(namespace)
+
+  private val HardenedGlassItem = Item(Item.Settings())
+
+  override fun onInitialize() {
+    // This code runs as soon as Minecraft is in a mod-load-ready state.
+    // However, some things (like resources) may still be uninitialized.
+    // Proceed with mild caution.
+    Registry.register(Registries.ITEM, Identifier(namespace, "hardened_glass"), HardenedGlassItem)
+  }
 }
