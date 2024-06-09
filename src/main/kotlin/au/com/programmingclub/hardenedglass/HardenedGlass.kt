00638f675b85586a8cbac0bd1d1a13c93d26602f
Joshua Wierenga <joshuagit@joshuawierenga.tech>
2024-06-10 01:34:41 +1000

    Add basic block
    Switched the item for a block item which allows placing the item as the new block.
    Currently stand in textures are used for both the block and the item.
    Additionally breaking the block currently does nothing, it should drop the item if
    using a silk touch pickaxe.

--- src/main/kotlin/au/com/programmingclub/hardenedglass/HardenedGlass.kt
+++ src/main/kotlin/au/com/programmingclub/hardenedglass/HardenedGlass.kt
@@ -2,6 +2,9 @@ package au.com.programmingclub.hardenedglass
 
 import net.fabricmc.api.ModInitializer
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents
+import net.minecraft.block.AbstractBlock
+import net.minecraft.block.Block
+import net.minecraft.item.BlockItem
 import net.minecraft.item.Item
 import net.minecraft.item.ItemGroups
 import net.minecraft.util.Identifier
@@ -14,13 +17,20 @@ object HardenedGlass : ModInitializer {
 
   private val logger = LoggerFactory.getLogger(namespace)
 
-  private val HardenedGlassItem = Item(Item.Settings())
+  private val HardenedGlassIdentifier = Identifier(namespace, "hardened_glass")
+  private val HardenedGlassBlock = Block(
+    AbstractBlock.Settings.create()
+    .hardness(10f)
+    .resistance(9f)
+  )
+  private val HardenedGlassItem = BlockItem(HardenedGlassBlock, Item.Settings())
 
   override fun onInitialize() {
     // This code runs as soon as Minecraft is in a mod-load-ready state.
     // However, some things (like resources) may still be uninitialized.
     // Proceed with mild caution.
-    Registry.register(Registries.ITEM, Identifier(namespace, "hardened_glass"), HardenedGlassItem)
+    Registry.register(Registries.BLOCK, HardenedGlassIdentifier, HardenedGlassBlock)
+    Registry.register(Registries.ITEM, HardenedGlassIdentifier, HardenedGlassItem)
 
     ItemGroupEvents.modifyEntriesEvent(ItemGroups.COLORED_BLOCKS).register({content ->
       content.add(HardenedGlassItem)
