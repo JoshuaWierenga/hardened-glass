018d4869334fa224eb87c0c110590bfcecc77068
Joshua Wierenga <joshuagit@joshuawierenga.tech>
2024-06-10 23:17:06 +1000

    Share more settings with glass
    Hardened glass now has the some sounds as glass.

--- src/main/kotlin/au/com/programmingclub/hardenedglass/HardenedGlass.kt
+++ src/main/kotlin/au/com/programmingclub/hardenedglass/HardenedGlass.kt
@@ -3,6 +3,7 @@ package au.com.programmingclub.hardenedglass
 import net.fabricmc.api.ModInitializer
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents
 import net.minecraft.block.AbstractBlock
+import net.minecraft.block.Blocks.GLASS
 import net.minecraft.block.TransparentBlock
 import net.minecraft.item.BlockItem
 import net.minecraft.item.Item
@@ -10,19 +11,15 @@ import net.minecraft.item.ItemGroups
 import net.minecraft.util.Identifier
 import net.minecraft.registry.Registries
 import net.minecraft.registry.Registry
-import org.slf4j.LoggerFactory
 
 object HardenedGlass : ModInitializer {
   private val namespace = "hardenedglass"
 
-  private val logger = LoggerFactory.getLogger(namespace)
-
   private val HardenedGlassIdentifier = Identifier(namespace, "hardened_glass")
   public val HardenedGlassBlock = TransparentBlock(
-    AbstractBlock.Settings.create()
+    AbstractBlock.Settings.copy(GLASS)
     .hardness(10f)
     .resistance(9f)
-    .nonOpaque()
   )
   private val HardenedGlassItem = BlockItem(HardenedGlassBlock, Item.Settings())
 
